dc98ed782fd49a28b7f8ef5d9d07e864ae197a6d
Rahul Agrawal <rahulunlimited@gmail.com>
2021-04-15 09:27:42 +1000

    Update UserRoles.sql

--- sql/UserRoles.sql
+++ sql/UserRoles.sql
@@ -1,9 +1,9 @@
+/*Script shows user roles*/
 SELECT DP1.name AS DatabaseRoleName,   
    isnull (DP2.name, 'No members') AS DatabaseUserName   
+   ,dp1.type
+   ,dp1.type_desc
  FROM sys.database_role_members AS DRM  
- RIGHT OUTER JOIN sys.database_principals AS DP1  
-   ON DRM.role_principal_id = DP1.principal_id  
- LEFT OUTER JOIN sys.database_principals AS DP2  
-   ON DRM.member_principal_id = DP2.principal_id  
-WHERE DP1.type = 'R'
+ RIGHT OUTER JOIN sys.database_principals AS DP1 ON DRM.role_principal_id = DP1.principal_id  
+ LEFT OUTER JOIN sys.database_principals AS DP2 ON DRM.member_principal_id = DP2.principal_id  
 ORDER BY DP1.name;

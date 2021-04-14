684188bc315a83c7668875bb10988ceaef548404
Rahul Agrawal <rahulunlimited@gmail.com>
2021-04-15 09:31:29 +1000

    Update SchemaPermissions.sql

--- sql/SchemaPermissions.sql
+++ sql/SchemaPermissions.sql
@@ -1,8 +1,11 @@
+/*Script shows permissions on Schema for Users and Roles */
 SELECT pr.principal_id, pr.name, pr.type_desc,   
     pr.authentication_type_desc, pe.state_desc   
 	,SCHEMA_NAME(pe.major_id) AS SchemaName
 	,pe.PERMISSION_NAME
+	,pe.major_id
 FROM sys.database_principals AS pr  
 JOIN sys.database_permissions AS pe  
     ON pe.grantee_principal_id = pr.principal_id  
 where pr.principal_id > 0 --Principal ID O is for user public
+and SCHEMA_NAME(pe.major_id) is not null

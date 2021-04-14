33beeb28f82b678af9e0e862f32a2a32c88beaa1
Rahul Agrawal <rahulunlimited@gmail.com>
2021-04-15 09:26:33 +1000

    Update UserPermission.sql

--- sql/UserPermission.sql
+++ sql/UserPermission.sql
@@ -1,16 +1,20 @@
-
+--Script shows user permissions
 SELECT DISTINCT 
 	pr.principal_id, 
 	pr.name AS [UserName], 
 	pr.type_desc AS [User_or_Role], 
 	pr.authentication_type_desc AS [Auth_Type], 
 	pe.state_desc,
-	pe.permission_name, 
+	isnull(pe.permission_name, '') permission, 
 	pe.class_desc, 
 	pr.type,
+	s.name AS SchemaName,
 	o.[name] AS 'Object' 
 FROM sys.database_principals AS pr 
 LEFT JOIN sys.database_permissions AS pe ON pe.grantee_principal_id = pr.principal_id
 LEFT JOIN sys.objects AS o on (o.object_id = pe.major_id)
-WHERE pr.type not in ('R')
---R is database role
+LEFT JOIN sys.schemas s ON s.schema_id = o.schema_id
+where 1 = 1
+and (pr.name not like 'db_%') 
+and pr.name not in ('guest', 'public', 'sys')
+ORDER BY UserName
